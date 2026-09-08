1a22f81ad9ee8da045bf5d9359e901aa7b99ae49
Haşim Üner <hasimuener@gmail.com>
2026-09-08 20:45:54 +0200

    Mirror privacy storage MIME allowlist migration

---
+++ supabase/migrations/20260908204500_privacy_storage_mime_allowlist.sql
@@ -0,0 +1,25 @@
+update storage.buckets
+set allowed_mime_types = array[
+  'application/pdf',
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'image/gif',
+  'video/mp4',
+  'video/webm',
+  'video/quicktime',
+  'text/plain',
+  'text/csv',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/vnd.ms-powerpoint',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  'application/rtf',
+  'application/vnd.oasis.opendocument.text',
+  'application/vnd.oasis.opendocument.spreadsheet',
+  'application/vnd.oasis.opendocument.presentation',
+  'application/zip'
+]::text[]
+where id = 'project-files';
